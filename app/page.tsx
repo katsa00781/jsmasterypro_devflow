bad90e1e099d5ba93cce89aaa62156ae370890f1
Zsolt Kacsor <katsa007@gmail.com>
2024-11-01 19:36:02 +0100

    ESlint & prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 const Home = () => {
   return (
-   <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Next.js</h1>
+   <h1 className="text-3xl font-black text-white">
+    Welcome to the world of Next.js</h1>
   );
 }
 
