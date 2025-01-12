e226ccf547651e2523ee0367ee31ad56c7849827
Zsolt Kacsor <katsa007@gmail.com>
2025-01-12 08:21:21 +0100

    Create userModel

---
+++ lib/database/user.model.ts
@@ -0,0 +1,30 @@
+
+import { Schema, models, model } from "mongoose";
+
+export interface IUser {
+    name: string;
+    userName: string;
+    email: string;
+    bio?: string;
+    image: string;
+    location?: string;
+    portfolio?: string;
+    reputation?: number;
+}
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    userName: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    bio: {type: String},
+    image: {type: String, required: true},
+    location: {type: String},
+    portfolio: {type: String},
+    reputation: {type: Number, default: 0},
+}, {timestamps: true});
+
+
+
+const User = models?.user || model<IUser>('User', userSchema);
+
+export default User;
